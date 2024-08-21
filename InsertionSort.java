5426ec78ab1f1a71d994354e6e21486506858d68
amitchell94 <andymitchell1@live.co.uk>
2024-08-21 15:27:42 -0500

    fixed bug and added comments to make sure I understand the algo

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,13 +6,26 @@ class InsertionSort {
         System.out.println("Before: " + arrayToString(array));
 
         for (int j = 1; j <= array.length - 1; j++) {
+            //j is the index of the number to be inserted
+
+            //key is the value to be sorted
             int key = array[j];
 
+            //i indicates the last index of the sorted numbers up to this point (or the first element if this is first run). 
             int i = j - 1;
-            while (i > 0 && array[i] > key) {
+
+            //Now we compare the key with the last sorted element, 
+            //if key is less than the element at i, 
+            //we subtract 1 from i and continue doing this until we find a spot (or we hit first element)
+            //we also shift the value at position i, one to the right each iteration of the whle loop, 
+            //this makes space for key when we find its spot. 
+            while (i >= 0 && array[i] > key) {
+
                 array[i + 1] = array[i];
                 i = i - 1;
             }
+
+            //finally, we insert the key in the spot after the index i.
             array[i + 1] = key;
         }
 
